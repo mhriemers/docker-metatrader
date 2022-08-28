1e9a5a81cc7db8cfe110483fb6c9d3051e9da8b3
Martijn Riemers <martijn@riemers.com>
2022-08-28 21:21:05 +0200

    Switch to double square brackets

--- terminal.sh
+++ terminal.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-if [ -z "${MT_VERSION}" ]; then
+if [[ -z "${MT_VERSION}" ]]; then
   echo "Missing MetaTrader version"
   exit 1
 fi
 
-if [ -z "${MT_INSTALLATION}" ]; then
+if [[ -z "${MT_INSTALLATION}" ]]; then
   echo "Missing MetaTrader installation"
   exit 1
 fi
 
-if [ ! -r "${MT_INSTALLATION}" ]; then
+if [[ ! -r "${MT_INSTALLATION}" ]]; then
   echo "MetaTrader installation is not readable!"
   echo "Current user: $(id)"
   echo "Directory permissions: $(ls -nd "${MT_INSTALLATION}")"
   exit 1
 fi
 
-if [ "${MT_VERSION}" == "4" ]; then
+if [[ "${MT_VERSION}" == "4" ]]; then
   exec xvfb-run -a wine "${MT_INSTALLATION}terminal.exe" "$@"
-elif [ "${MT_VERSION}" == "5" ]; then
+elif [[ "${MT_VERSION}" == "5" ]]; then
   exec xvfb-run -a wine "${MT_INSTALLATION}terminal64.exe" "$@"
 else
   echo "Unsupported MetaTrader version!"
