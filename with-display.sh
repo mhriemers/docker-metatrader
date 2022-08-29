9861b4f411be0d8bbfb6e3717d2c28f5027001fe
Martijn Riemers <martijn@riemers.com>
2022-08-29 21:05:29 +0200

    Add vnc and fluxbox using script

--- with-display.sh
+++ with-display.sh
@@ -2,15 +2,29 @@
 
 export DISPLAY=:0
 
-x11vnc -bg -forever -nopw -quiet -display WAIT$DISPLAY
-Xvfb $DISPLAY -screen 0 1920x1080x32 +extension RANDR &
-sleep 1
+x11vnc -bg -forever -nopw -quiet -display WAIT$DISPLAY &
 fluxbox 2>/dev/null &
-xdpyinfo
+Xvfb $DISPLAY -screen 0 1920x1080x24 +extension RANDR &
+
+MAX_ATTEMPTS=120
+COUNT=0
+echo -n "Waiting for Xvfb to be ready..."
+while ! xdpyinfo -display "${DISPLAY}" >/dev/null 2>&1; do
+  echo -n "."
+  sleep 0.50s
+  COUNT=$(( COUNT + 1 ))
+  if [[ ${COUNT} -ge ${MAX_ATTEMPTS} ]]; then
+    echo "  Gave up waiting for X server on ${DISPLAY}"
+    exit 1
+  fi
+done
+echo "  Done - Xvfb is ready!"
 
 "$@"
 RET_VAL=$?
 
-pkill x11vnc Xvfb fluxbox || true
+pkill x11vnc || true
+pkill Xvfb || true
+pkill fluxbox || true
 
 exit $RET_VAL
